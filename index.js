c899dadc701836288ca89b21c7447f13e68ed2a3
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-12 21:01:12 +0300

    fix cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { verifyToken } from "./src/utils/VerifyToken.js";
 import initApp from "./src/index.routes.js";
 const app = express();
 dotenv.config();
 
 app.use(cors());
-// setup port and the baseUrl
-// initApp(app, express)
+
+initApp(app, express);
+
 
 const port = +process.env.PORT;
 app.listen(port, () => console.log(`App listening on port:${port}!`));
