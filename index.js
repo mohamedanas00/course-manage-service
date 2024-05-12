d503241eddf0afb0a0e31be792a06386e6477a3a
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-12 21:00:34 +0300

    fix cors

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ import { verifyToken } from "./src/utils/VerifyToken.js";
 import initApp from "./src/index.routes.js";
 const app = express();
 dotenv.config();
-// Verify token asynchronously
-const decode = verifyToken(
-  "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJuYW1lIjoiSGFzc2FuIiwiaWQiOjUsImVtYWlsIjoiaGFzc2FuMDA3QGdtYWlsLmNvbSIsImV4cCI6MTc0NjkxMTA2NH0.9FPffo-1Y8jf-HMJDissz5p4wgZDUmmJRz3yGcZOUYs"
-);
-console.log("Decoded Token:", decode.role,decode.id);
-initApp(app, express);
 
-
-//cors
 app.use(cors());
 // setup port and the baseUrl
 // initApp(app, express)
