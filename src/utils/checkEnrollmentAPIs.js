dc821152d44839cb879bf73cb753e12501044df2
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-15 04:05:57 +0300

    delete logs

--- src/utils/checkEnrollmentAPIs.js
+++ src/utils/checkEnrollmentAPIs.js
@@ -43,7 +43,6 @@ export async function checkEnrollmentWithCircuitBreaker(courseId, userId) {
     if (failedRequestsCount >= failedRequestsThreshold) {
       breaker.open();
     }
-    console.error("Error calling checkEnrollment via CircuitBreaker:", error);
     return error.message;
   }
 }
